0fcec948ad995e156261bb1a91bca057679d150c
Fabian Ehlert <fabian@fabianehlert.com>
2016-02-05 20:11:34 +0100

    Added logic and sample data

--- FELocalizer-Demo/ViewController.swift
+++ FELocalizer-Demo/ViewController.swift
@@ -12,14 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let file = NSBundle.mainBundle().pathForResource("Localizer", ofType: "json") {
+            FELocalizer.shared.setFilePath(file)
+            
+            let translation = FELocalizer.shared.localized("K_HELLO")
+            print("Translation : \(translation)")
+        }
     }
-
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
-
--- FELocalizer.swift
+++ FELocalizer.swift
@@ -8,6 +8,58 @@
 
 import Foundation
 
+extension String {
+    func firstComponentTillSeparator() -> String {
+        if let x = self.componentsSeparatedByString("-").first { return x }
+        return self
+    }
+}
+
 class FELocalizer {
 
+    static let shared = FELocalizer()
+    private var json: [String: AnyObject]!
+    
+    /// Sets the path for the JSON file that contains the translation strings
+    func setFilePath(path: String) {
+        if let jsonData = NSData(contentsOfFile: path) {
+            do {
+                json = try NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.MutableContainers) as! [String: AnyObject]
+            } catch let error {
+                print(error)
+            }
+        }
+
+    }
+    
+    /// Returns a localized String
+    func localized(key: String, language: String! = nil) -> String {
+        if let _ = language {
+            return string(key, language: language)
+        } else {
+            return string(key, language: languageISOCode())
+        }
+    }
+    
+    private func string(key: String, language: String) -> String {
+        if let _ = json {
+            if let object = json[key] {
+                if let l = object[languageISOCode()] {
+                    if let l: String = l as? String {
+                        return l
+                    }
+                }
+            }
+        }
+        return ""
+    }
+    
+    /// Returns the ISO only code (2 or 3 character) of the currently preferred system language
+    func languageISOCode() -> String {
+        if let language = NSLocale.preferredLanguages().first {
+            return language.firstComponentTillSeparator()
+        }
+        return "en"
+    }
+    
 }
