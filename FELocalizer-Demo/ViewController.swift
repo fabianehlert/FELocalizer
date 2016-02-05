8b1b5fc3eb43c37b2e7a3da47d3c4a2f97890f7c
Fabian Ehlert <fabian@fabianehlert.com>
2016-02-05 16:03:10 +0100

    Initial Commit

---
+++ FELocalizer-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FELocalizer-Demo
+//
+//  Created by Fabian Ehlert on 05.02.16.
+//  Copyright © 2016 Fabian Ehlert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
