78bbc51b90a6879a1dd0382d547978732543465a
Fabian Ehlert <fabian@fabianehlert.com>
2016-02-05 16:05:23 +0100

    Created Files

---
+++ FELocalizer.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  FELocalizer-Demo
+//
+//  Created by Fabian Ehlert on 05.02.16.
+//  Copyright © 2016 Fabian Ehlert. All rights reserved.
+//
+
+import Foundation
+
+class FELocalizer {
+
+}
